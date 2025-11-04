bcba32077f5ac63ff2568246d912c61d1023ee87
Itzmeyogesh <maskeyogeish@gmail.com>
2025-11-04 12:08:27 +0530

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+<<<<<<< HEAD
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
         'grid': "linear-gradient(to right, rgba(255,255,255,.06) 1px, transparent 1px), linear-gradient(to bottom, rgba(255,255,255,.06) 1px, transparent 1px)"
       }
+=======
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      dropShadow: {
+        glow: '0 0 10px rgba(34,211,238,0.6)',
+      },
+>>>>>>> 971bdbf (Update)
     },
   },
   plugins: [],
